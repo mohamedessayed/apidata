bd658f447f4aa5fc8dbccca9fe1eb5a7c86e1334
mohamedessayed <mlovely9316@gmail.com>
2025-11-03 22:07:37 +0200

    first comment

---
+++ js/api.js
@@ -0,0 +1,104 @@
+function seeFood() {
+    return new Promise(function (resolve) {
+        var myHttp = new XMLHttpRequest();
+
+        myHttp.open('get', 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood');
+        myHttp.send()
+
+        myHttp.addEventListener('readystatechange', function () {
+            if (myHttp.readyState === 4 && myHttp.status === 200) {
+                var responseDate = JSON.parse(myHttp.responseText)
+
+                console.log('See food', responseDate);
+
+                resolve() //callback function
+
+            }
+        })
+    })
+}
+
+function beef() {
+    return new Promise(function (resolve) {
+        var myHttp = new XMLHttpRequest();
+
+        myHttp.open('get', 'https://www.themealdb.com/api/json/v1/1/filter.php?c=beef');
+        myHttp.send()
+
+        myHttp.addEventListener('readystatechange', function () {
+            if (myHttp.readyState === 4 && myHttp.status === 200) {
+                var responseDate = JSON.parse(myHttp.responseText)
+
+                console.log('Beef', responseDate);
+
+                resolve()
+            }
+        })
+    })
+}
+
+function chicken() {
+
+    return new Promise(function (resolve) {
+        var myHttp = new XMLHttpRequest();
+
+        myHttp.open('get', 'https://www.themealdb.com/api/json/v1/1/filter.php?c=chicken');
+        myHttp.send()
+
+        myHttp.addEventListener('readystatechange', function () {
+            if (myHttp.readyState === 4 && myHttp.status === 200) {
+                var responseDate = JSON.parse(myHttp.responseText)
+
+                console.log('chicken', responseDate);
+                resolve()
+            }
+        })
+    })
+}
+
+function end() {
+    console.log('end');
+}
+
+
+
+
+// seeFood(function(){
+//     beef(function(){
+//         chicken(end)
+//     })
+// })
+
+
+// seeFood()
+// .then(beef)
+// .then(chicken)
+// .then(end)
+
+
+fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+.then(function (res) {
+    return res.json();
+})
+.then(function (Data) {
+    console.log(Data);
+})
+.then(function(){
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=beef')
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (Data) {
+        console.log(Data);
+    })
+})
+.then(function(){
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=chicken')
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (Data) {
+        console.log(Data);
+    })
+})
+    
